a62506150c7a5189f5b1459d7ec86733a9c2d994
JDJaramillo10 <juan.jaramillo@uees.edu.ec>
2024-07-31 15:36:58 -0500

    commit carpetas

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ num = os.environ.get("INPUT_NUM")
 if num:
     try:
         num = int(num)
-    except Exception
-    exit('El número ingresado no es entero')
+    except Exception:
+        exit('El número ingresado no es entero')
 else:
     num = 1
 
 if "GITHUB_OUTPUT" in os.environ :
-        with open(os.environ["GITHUB_OUTPUT"], a) as f:
-             print("{0} = {1}".format('result', num+3), file=f)
+    with open(os.environ["GITHUB_OUTPUT"], a) as f:
+        print("{0} = {1}".format('result', num+3), file=f)
